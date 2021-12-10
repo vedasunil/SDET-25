e6b449bf818c7bb0cdba08de1be46ccb5fe18b95
veda <veda@LAPTOP-A3QVIB4R>
2021-12-10 10:36:48 +0530

    commit---1

---
+++ src/main/java/v3/Vtiger/App.java
@@ -0,0 +1,13 @@
+package v3.Vtiger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
